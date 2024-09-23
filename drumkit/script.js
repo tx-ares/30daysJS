5fefa427efc8137b92128ed685abc70c3959d3aa
tx-ares <twinadanz@gmail.com>
2024-09-23 15:52:02 -0500

    complete 1st iteration

--- drumkit/script.js
+++ drumkit/script.js
@@ -1,24 +1,48 @@
 console.log('* TX-HYPERION ONLINE *');
 
-function addSoundHandlers() {
-	// loop through all keys and add a keyevent listener
-	const keys = document.getElementsByClassName('key');
-	console.log(keys);
-	window.addEventListener('keydown', playSound);
+function addKeyListener() {
+	window.addEventListener('keydown', playSoundHandler);
 }
 
-function playSound(event) {
+function playSoundHandler(event) {
 	// get sounds
 	const angelDrum = document.getElementById('angelic-drum');
+	const arcadeBonus = document.getElementById('arcade-bonus');
+	const levelComplete = document.getElementById('level-complete');
+	const playerLose = document.getElementById('player-lose');
+	const bonusAlert = document.getElementById('bonus-alert');
+	const drumImpact = document.getElementById('drum-impact');
+	const subBass = document.getElementById('sub-bass');
+	const unlockGame = document.getElementById('unlock-game');
+	console.log(event)
 
-	console.log('playing', event);
 	switch (event.key) {
 		case 'f':
 			angelDrum.play();
 			break;
+		case 'd':
+			arcadeBonus.play();
+			break;
+		case 's':
+			levelComplete.play();
+			break;
+		case 'a':
+			playerLose.play();
+			break;
+		case 'j':
+			bonusAlert.play();
+			break;
+		case 'k':
+			drumImpact.play();
+			break;
+		case 'l':
+			subBass.play();
+			break;
+		case ';':
+			unlockGame.play();
+			break;
 		default:
-			console.log(`Sorry, we are out of ${expr}.`);
 	}
 }
 
-addSoundHandlers();
+addKeyListener();
